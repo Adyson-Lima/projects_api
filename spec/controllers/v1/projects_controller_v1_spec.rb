ada5ca885d932548b6c7df146d88491ea8f5d65a
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-15 11:28:34 -0300

    12° commit - tdd controller: Consegue listar um project especifico e retornar status 200? RED

--- spec/controllers/v1/projects_controller_v1_spec.rb
+++ spec/controllers/v1/projects_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::ProjectsController, type: :controller do
       end
     end
 
+    describe "GET /api/v1/projects/id" do
+      it "Consegue listar um project especifico e retornar status 200?" do
+        get :show, params: {id: @project.id}
+          expect(response.body).to include_json(id: @project.id)
+          expect(response).to have_http_status(200)
+      end
+    end
+
 end
